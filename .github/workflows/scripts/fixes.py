f81f791ad9d1262f4e3e8ca245996829039259ac
Ardit <ardittristan@gmail.com>
2022-12-04 00:45:05 +0100

    try new fix

--- .github/workflows/scripts/fixes.py
+++ .github/workflows/scripts/fixes.py
@@ -1,8 +1,8 @@
 import typing
-from xml.etree.ElementTree import Element, SubElement, parse, tostring
+from xml.etree.ElementTree import Element, SubElement, parse
 from os import getcwd, path
-import os
 
+#region missing virtualtreeview_package
 filepath = path.join(path.abspath(getcwd()), "Cheat Engine/cheatengine.lpi")
 
 tree = parse(filepath)
@@ -19,3 +19,28 @@ if (virtualtreeview_package == None):
   package = SubElement(packageItem, "PackageName")
   package.set("Value", "laz.virtualtreeview_package")
   tree.write(filepath, encoding="utf8")
+
+#endregion
+
+#region doubletoextended fix
+filepath = path.join(path.abspath(getcwd()),
+                     "Cheat Engine/frmModifyRegsitersUnit.pas")
+
+with open(filepath, "r") as f:
+  data = f.read()
+  data = data.replace(
+    """              if reg^.size=10 then doubletoextended(@d, reg^.getPointer(regeditinfo.context)) else
+              if reg^.size=4 then copymemory(reg^.getPointer(regeditinfo.context),@f,sizeof(f)) else
+              if reg^.size=8 then copymemory(reg^.getPointer(regeditinfo.context),@d,sizeof(d))""",
+    """              {$ifdef cpux86_64}
+              if reg^.size=10 then doubletoextended(@d, reg^.getPointer(regeditinfo.context)) else
+              {$else}
+              if reg^.size=10 then copymemory(reg^.getPointer(regeditinfo.context),@d,10) else
+              {$endif}
+              if reg^.size=4 then copymemory(reg^.getPointer(regeditinfo.context),@f,sizeof(f)) else
+              if reg^.size=8 then copymemory(reg^.getPointer(regeditinfo.context),@d,sizeof(d))"""
+  )
+
+with open(filepath, "w") as f:
+  f.write(data)
+#endregion
