1c8861ec69faf2122df6b540a5dbf1305d67d141
Ardit <ardittristan@gmail.com>
2022-12-26 04:12:39 +0100

    switch over to powershell

--- .github/workflows/scripts/getFpcVersion.js
+++
@@ -1,7 +0,0 @@
-const { readFileSync } = require("fs");
-const { join } = require("path");
-
-let fileContent = readFileSync(join(process.env.GITHUB_WORKSPACE, "README.md"), { encoding: "utf-8", flag: "r" });
-let versions = fileContent.match(/lazarus-(?<lazarus>[0-9]+\.[0-9]+\.[0-9]+)-fpc-(?<fpc>[0-9]+\.[0-9]+\.[0-9]+)-cross-i386-win32-win64\.exe/);
-
-console.log(versions.groups.fpc);
--- .github/workflows/scripts/getPackages.js
+++
@@ -1,7 +0,0 @@
-const { readFileSync } = require("fs");
-const { join } = require("path");
-
-let fileContent = readFileSync(join(process.env.GITHUB_WORKSPACE, "Cheat Engine/cheatengine.lpi"), { encoding: "utf-8", flag: "r" });
-let dependencies = fileContent.match(/(?<=<PackageName Value=").*?(?="\/>)/g).join("{0}");
-
-console.log(dependencies);
