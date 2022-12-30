faa4120b9bdfd8cbca89b74e8f149bf7e16540e1
Ardit <ardittristan@gmail.com>
2022-12-30 02:36:50 +0100

    revert cache patch

--- .github/workflows/scripts/patch-cache.js
+++
@@ -1,8 +0,0 @@
-const filePath = require("path").join(__dirname, "../actions/cache/dist/restore/index.js");
-
-require("fs").writeFileSync(
-  filePath,
-  require("fs")
-    .readFileSync(filePath, { encoding: "utf8" })
-    .replace(/(function downloadCache\(archiveLocation, archivePath, options\) {).*?(^})/ms, "$1$2")
-);
